0a0a181b2a5f3d99fad61eeb323f48f23b1388ee
Amrit-2708 <rrajamrit.21@gmail.com>
2025-08-09 20:29:16 +0530

    changed backend link from localhost to deployed on render

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+const url = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const socket = io(url);
 export default socket;
--- server/index.js
+++ server/index.js
@@ -116,5 +116,6 @@ function getPollResults() {
     return counts;
 }
 
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
